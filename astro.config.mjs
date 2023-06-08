76fbd8586c9b70a3c5873a604b1185356ed455c4
Asandei Stefan <asandei.stefanel@gmail.com>
2023-06-08 10:34:36 +0300

    idk fix stuff & update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,15 @@ import { defineConfig } from "astro/config";
 import AstroPWA from "@vite-pwa/astro";
 import lit from "@astrojs/lit";
 import sitemap from "@astrojs/sitemap";
+import { readFileSync } from "fs";
 
 export default defineConfig({
-  site: "https://stefan-asandei.cf",
+  site: "https://stefan-asandei.netlify.app",
   integrations: [sitemap(), AstroPWA(), lit()],
   markdown: {
     shikiConfig: {
       theme: JSON.parse(
-        fs.readFileSync("./public/themes/catppuccin-macho.json", "utf-8")
+        readFileSync("./public/themes/catppuccin-macho.json", "utf-8")
       ),
       langs: [],
       wrap: true,
