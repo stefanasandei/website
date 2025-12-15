d8886cf8493b9f9bdfb8c108ed860e16a02aa862
Asandei Stefan-Alexandru <asandei.stefanel@gmail.com>
2025-12-15 18:08:53 +0200

    fix projects page

--- src/components/ProjectsSection.svelte
+++ src/components/ProjectsSection.svelte
@@ -5,13 +5,15 @@
     const projects = [
         {
             name: "AI Tasks Solutions",
-            role: "https://github.com/stefanasandei/roai-solved",
+            slug: "ai-tasks-solutions",
+            github: "https://github.com/stefanasandei/roai-solved",
             description:
                 "TLDR: Solved and explained competitive AI tasks from IOAI, National Olympiads (Romania, Poland, Russia, etc.) and other contests.",
         },
         {
             name: "Orion: an agent for personal knowledge base management",
-            role: "https://github.com/stefanasandei/orion",
+            slug: "orion",
+            github: "https://github.com/stefanasandei/orion",
             description:
                 "TLDR: fullstack web application that uses vector RAG on personal documents to quickly answer & summarize content",
         },
@@ -25,13 +27,17 @@
         {#each projects as project}
             <div class="space-y-2">
                 <a
-                    href="#"
+                    href="/projects/{project.slug}"
                     class="text-base font-semibold hover:text-primary transition-colors"
                 >
                     {project.name}
                 </a>
-                {#if project.role}
-                    <p class="text-sm text-muted-foreground">{project.role}</p>
+                {#if project.github}
+                    <a
+                        href={project.github}
+                        class="text-sm text-muted-foreground hover:text-primary transition-colors"
+                        >{project.github}</a
+                    >
                 {:else}
                     <p class="py-1"></p>
                 {/if}
@@ -40,5 +46,5 @@
         {/each}
     </div>
 
-    <CoolLink href="#">all projects</CoolLink>
+    <CoolLink href="/projects">all projects</CoolLink>
 </div>
