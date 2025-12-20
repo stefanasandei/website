d90ee178eac8a83ad111d7f9b001db23c094fc2d
Asandei Stefan-Alexandru <asandei.stefanel@gmail.com>
2025-12-20 18:54:54 +0200

    todo article

--- src/components/SectionTitle.svelte
+++ src/components/SectionTitle.svelte
@@ -4,22 +4,22 @@
 </script>
 
 {#if haveBorder}
-    <h2 class="text-lg font-bold uppercase mb-4 pb-2 border-b border-border">
+    <h2 class="text-lg font-bold mb-4 pb-2 border-b border-border">
         <span
             class={`text-primary font-extrabold ${large ? "text-4xl" : "text-2xl"}`}
             >*</span
         >
-        <span class={`lowercase ${large ? "text-4xl" : "text-2xl"}`}>
+        <span class={`${large ? "text-4xl" : "text-2xl"}`}>
             <slot />
         </span>
     </h2>
 {:else}
-    <h2 class="text-lg font-bold uppercase pb-2">
+    <h2 class="text-lg font-bold pb-2">
         <span
             class={`text-primary font-extrabold ${large ? "text-4xl" : "text-2xl"}`}
             >*</span
         >
-        <span class={`lowercase ${large ? "text-4xl" : "text-2xl"}`}>
+        <span class={`${large ? "text-4xl" : "text-2xl"}`}>
             <slot />
         </span>
     </h2>
